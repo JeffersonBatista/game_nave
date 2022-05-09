7547d6fe47cba1c912b0c3b31fd1b01469cc8806
Jefferson_Batista <jefferson_tecnico@hotmail.com>
2022-05-09 20:11:59 -0300

    explosion final

--- assets/js/script.js
+++ assets/js/script.js
@@ -209,6 +209,19 @@ function start() { // Inicio da função start()
             reposicionaAmigo();
             $("#amigo").remove();
         }
+
+        //Inimigo2 com o amigo
+		
+        if (colisao6.length>0) {
+	    
+        amigoX = parseInt($("#amigo").css("left"));
+        amigoY = parseInt($("#amigo").css("top"));
+        explosao3(amigoX,amigoY);
+        $("#amigo").remove();
+            
+        reposicionaAmigo();
+            
+        }
         
     }//fim da função colisao()
 
@@ -250,6 +263,22 @@ function start() { // Inicio da função start()
                 
         }
     } // Fim da função explosao2()
+
+    //Explosão3
+	
+    function explosao3(amigoX,amigoY) {
+        $("#fundoGame").append("<div id='explosao3' class='anima4'></div");
+        $("#explosao3").css("top",amigoY);
+        $("#explosao3").css("left",amigoX);
+        var tempoExplosao3=window.setInterval(resetaExplosao3, 1000);
+        function resetaExplosao3() {
+        $("#explosao3").remove();
+        window.clearInterval(tempoExplosao3);
+        tempoExplosao3=null;
+            
+        }
+    
+    } // Fim da função explosao3
 
     //Reposiciona Amigo
 	
