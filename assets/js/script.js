4553b174992e03efec9404048f1581117dc19183
Jefferson_Batista <jefferson_tecnico@hotmail.com>
2022-05-09 16:18:39 -0300

    move helicopter

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,16 +9,63 @@ function start() { // Inicio da função start()
 
     //Principais variáveis do jogo
 	var jogo = {}
+    var TECLA = {
+        W: 87,
+	    S: 83,
+	    D: 68
+    }
+        jogo.pressionou = [];
+
+    //Verifica se o usuário pressionou alguma tecla	
+	$(document).keydown(function(e){
+        jogo.pressionou[e.which] = true;
+        });
+    
+    
+        $(document).keyup(function(e){
+           jogo.pressionou[e.which] = false;
+        });
+    
 	//Game Loop
 	jogo.timer = setInterval(loop,30);
 	function loop() {
 	movefundo();
+    movejogador();
 	} // Fim da função loop()
 } // Fim da função start
 
-//Função que movimenta o fundo do jogo
-	
+//Função que movimenta o fundo do jogo	
 function movefundo() {
 	esquerda = parseInt($("#fundoGame").css("background-position"));
 	$("#fundoGame").css("background-position",esquerda-2);
 } // fim da função movefundo()
+
+function movejogador() {
+	
+	if (jogo.pressionou[TECLA.W]) {
+		var topo = parseInt($("#jogador").css("top"));
+		$("#jogador").css("top",topo-10);
+        
+        if (topo<=0) {
+		
+            $("#jogador").css("top",topo+10);
+        }
+	}
+	
+	if (jogo.pressionou[TECLA.S]) {
+		
+		var topo = parseInt($("#jogador").css("top"));
+		$("#jogador").css("top",topo+10);
+        
+        if (topo>=435) {	
+            $("#jogador").css("top",topo-10);
+                
+        }
+	}
+	
+	if (jogo.pressionou[TECLA.D]) {
+		
+		//Chama função Disparo	
+	}
+
+	} // fim da função movejogador()
